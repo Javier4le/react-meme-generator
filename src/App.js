bf6f75f8355e3fa6660762f836c849030b47ce1a
Javier Rojas <javier4le.dev@gmail.com>
2022-01-26 03:50:54 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import html2canvas from 'html2canvas';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [linea1, setLinea1] = useState("");
+   const [linea2, setLinea2] = useState("");
+   const [imagen, setImagen] = useState("");
+
+   const onChangeLinea1 = function(evento) {
+      // alert(evento.target.value)
+      setLinea1(evento.target.value)
+   }
+
+   const onChangeLinea2 = function(evento) {
+      // alert(evento.target.value)
+      setLinea2(evento.target.value)
+   }
+
+   const onChangeImagen = function(evento) {
+      setImagen(evento.target.value)
+   }
+
+   const onClickExportar = function(evento) {
+      alert("Exportar")
+      html2canvas(document.querySelector("#meme")).then(canvas => {
+         let img = canvas.toDataURL("image/png");
+         let link = document.createElement("a");
+         link.download = "meme.png";
+         link.href = img;
+         link.click();
+      });
+   }
+
+   return (
+      <div className="App">
+
+         <select onChange={onChangeImagen}>
+            <option value="fire">Casa en llamas</option>
+            <option value="futurama">Futurama</option>
+            <option value="history">History Channel</option>
+            <option value="matrix">Matrix</option>
+            <option value="philosoraptor">Philosoraptor</option>
+            <option value="smart">Smart Guy</option>
+         </select> <br />
+
+         <input onChange={onChangeLinea1} type="text" placeholder="Línea 1" /> <br />
+
+         <input onChange={onChangeLinea2} type="text" placeholder="Línea 2" /> <br />
+
+         <button onClick={onClickExportar}>Exportar</button>
+
+
+         <div className="meme" id="meme">
+            <span>{linea1}</span>
+            <span>{linea2}</span>
+            <img src={`/img/${imagen}.jpg`} />
+         </div>
+
+      </div>
+   );
 }
 
 export default App;
